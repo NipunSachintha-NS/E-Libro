bfc42400a9ee5e8d9083c5e2d02c126ac2293db4
nalin <rmnsbandara@std.appsc.sab.ac.lk>
2024-09-03 08:53:48 +0530

    Set up background styling with Tailwind CSS

---
+++ Frontend/E-libro/src/Pages/HomePage.jsx
@@ -0,0 +1,27 @@
+// File: src/components/WelcomePage.js
+
+import React from 'react';
+import './HomePage.css'; // Import the CSS file for styling
+import { Link } from 'react-router-dom';
+
+const HomePage = () => {
+  return (
+    <div className="Home-container">
+      <div className="Home-content">
+        <h1 className="Home-title">E-Libro</h1>
+        <p className="Home-message">
+          "Home to your personal e-library. Start managing your books!"
+        </p>
+        <button className="Home-button">Get Started</button>
+        <p className="login-link">
+          Do you have an account? <Link to ="/login">Log In</Link>
+        </p>
+      </div>
+      <div className="Home-image">
+        <img src="/src/assets/f89e8492ea5acb393bacb09c1a9699f2.jpeg" alt="E-Library illustration" />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
