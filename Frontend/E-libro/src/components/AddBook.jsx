e7b0d6159c5c553ebe144434ae3738c7fb02e989
nalin <rmnsbandara@std.appsc.sab.ac.lk>
2024-10-16 16:45:15 +0530

    Updated

--- Frontend/E-libro/src/components/AddBook.jsx
+++ Frontend/E-libro/src/components/AddBook.jsx
@@ -1,17 +1,42 @@
 import React from "react";
-import { BiFontSize } from "react-icons/bi";
-const styles = {
-  height: "60px",
-  width: "500px",
-  margin:"10px",
-  fontSize:"0.6rem",
-  padding:'10px'
-};
+
 function AddBook() {
   return (
-    <>
-      
-    </>
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="bg-blue-100 p-8 rounded-lg shadow-lg ">
+        <h2 className="text-2xl font-semibold text-blue-600 mb-6 text-center">
+          Book Details
+        </h2>
+        <form>
+          <input
+            type="text"
+            placeholder="Name"
+            className="block w-full mb-4 px-3 py-2 text-sm border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          <input
+            type="text"
+            placeholder="Title"
+            className="block w-full mb-4 px-3 py-2 text-sm border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          <input
+            type="text"
+            placeholder="Author"
+            className="block w-full mb-4 px-3 py-2 text-sm border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          <input
+            type="text"
+            placeholder="Description"
+            className="block w-full mb-4 px-3 py-2 text-sm border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          <button
+            type="submit"
+            className="block w-full bg-blue-500 text-white text-sm font-semibold py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+          >
+            Save
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
 
