0aaaf7104d48a5b6480ac4a3ccdd6b073445338d
Nipun Sachintha <lvnsachintha@std.appsc.sab.ac.lk>
2024-12-18 23:22:22 +0530

    Register Form Backend Operation

---
+++ Backend/index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const PORT = process.env.PORT;
+
+const express = require("express");
+const cors = require('cors');
+
+
+const app = express();
+const dbCon = require("../config/dbCon");
+const bookRouter = require("../routes/bookRoutes");
+const userRouter = require("../routes/userRoutes");
+
+app.use(express.json());
+app.use(cors());
+app.use("/", bookRouter);
+app.use("/users", userRouter);
+
+
+
+
+dbCon().then(async () => {
+  await app.listen(PORT, () => {
+    console.log("server is running");
+  });
+});
--- Frontend/E-libro/src/components/RegistrationForm.jsx
+++ Frontend/E-libro/src/components/RegistrationForm.jsx
@@ -28,3 +28,66 @@ const RegistrationForm = () => {
 };
 
 export default RegistrationForm;
+
+import { Link } from 'react-router-dom';
+import './RegistrationForm.css';
+import { useState } from 'react';
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
+function RegistrationForm  ()  {
+
+    const [name, setName] = useState('');
+    const [contactNo, setContactNo] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const navigate = useNavigate();
+    
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+        if (password !== confirmPassword) {
+          alert('Passwords do not match!');
+          return;
+        }
+        axios.post('http://localhost:3001/users/signup',{name,contactNo,password})
+        .then(result => {console.log(result)
+          alert('Registration successful!')
+        navigate('/login')
+        })
+        .catch(err => console.log(err))
+    }
+  
+  return (
+    <div className="registration-container">
+      <h2 className="registration-title">E-Libro</h2>
+      <form className="registration-form"  onSubmit={handleSubmit}>
+
+        <div className="form-group">
+          <input type="text" htmlFor='email' placeholder="UserName :" className="form-inputs"
+          onChange={(e) => setName(e.target.value)} />
+        </div>
+
+        <div className="form-group">
+          <input type="text" htmlFor='email' placeholder="ContactNo :" className="form-inputs" 
+          onChange={(e) => setContactNo(e.target.value)}/>
+        </div>
+
+        <div className="form-group">
+          <input type="password"  htmlFor='email'placeholder="Password :" className="form-inputs"
+          onChange={(e) => setPassword(e.target.value)} />
+        </div>
+
+        <div className="form-group">
+          <input type="password" htmlFor='email' placeholder="Confirm Password :" className="form-inputs" 
+          onChange={(e) => setConfirmPassword(e.target.value)}/>
+        </div>
+
+        <button type="submit" className="register-button">Register</button>
+        <p className="login-links">
+          Do you have an account? <Link to="/login">Log In</Link>
+        </p>
+      </form>
+    </div>
+  );
+};
