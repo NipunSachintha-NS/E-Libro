73f183db8554a86ff2aa8517bbb220407776a8cd
Nipun Sachintha <lvnsachintha@std.appsc.sab.ac.lk>
2024-12-24 19:10:09 +0530

    Register form Backend Operation

--- Backend/index.js
+++ Backend/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:Backend/index.js
 require("dotenv").config();
 const PORT = process.env.PORT;
 
@@ -22,4 +23,30 @@ dbCon().then(async () => {
   await app.listen(PORT, () => {
     console.log("server is running");
   });
+=======
+require("dotenv").config();
+const PORT = process.env.PORT;
+
+const express = require("express");
+const cors = require('cors');
+
+
+const app = express();
+const dbCon = require("../config/dbCon");
+const bookRouter = require("../routes/bookRoutes");
+const userRouter = require("../routes/userRoutes");
+
+app.use(express.json());
+app.use(cors());
+app.use("/", bookRouter);
+app.use("/users", userRouter);
+
+
+
+
+dbCon().then(async () => {
+  await app.listen(PORT, () => {
+    console.log("server is running");
+  });
+>>>>>>> 60ccefa (Register form Backend Operation):Backend/src/index.js
 });
--- Frontend/E-libro/src/components/RegistrationForm.jsx
+++ Frontend/E-libro/src/components/RegistrationForm.jsx
@@ -1,36 +1,5 @@
 import { Link } from 'react-router-dom';
 import './RegistrationForm.css';
-
-const RegistrationForm = () => {
-  return (
-    <div className="registration-container">
-      <h2 className="registration-title">E-Libro</h2>
-      <form className="registration-form">
-        <div className="form-group">
-          <input type="text" placeholder="UserName :" className="form-inputs" />
-        </div>
-        <div className="form-group">
-          <input type="text" placeholder="ContactNo :" className="form-inputs" />
-        </div>
-        <div className="form-group">
-          <input type="password" placeholder="Password :" className="form-inputs" />
-        </div>
-        <div className="form-group">
-          <input type="password" placeholder="Confirm Password :" className="form-inputs" />
-        </div>
-        <button type="submit" className="register-button">Register</button>
-        <p className="login-links">
-          Do you have an account? <Link to="/login">Log In</Link>
-        </p>
-      </form>
-    </div>
-  );
-};
-
-export default RegistrationForm;
-
-import { Link } from 'react-router-dom';
-import './RegistrationForm.css';
 import { useState } from 'react';
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
@@ -91,3 +60,6 @@ function RegistrationForm  ()  {
     </div>
   );
 };
+
+
+export default RegistrationForm
